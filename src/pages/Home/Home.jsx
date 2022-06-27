9181fa7008236baa0a3453fde7752a9b5f8ebf26
junaidapdev <junaidap.dev.com>
2022-06-27 10:53:15 +0530

    home page & profileSide component created

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import ProfileSide from '../../components/ProfileSide/ProfileSide'
+import './Home.css'
+
+const Home = () => {
+  return (
+    <div className="Home">
+        <ProfileSide/>
+        <div className="postSide">post</div>
+        <div className="rightSide">right</div>
+    </div>
+  )
+}
+
+export default Home
